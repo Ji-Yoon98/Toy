247bdacdef661e662b2ef2274bd31febca089c34
parkJi-yun <nmjk0123@naver.com>
2023-05-09 20:31:03 +0900

    section slide update

--- js/main.js
+++ js/main.js
@@ -15,3 +15,60 @@ searchInputEl.addEventListener('blur', function() {
   searchInputEl.setAttribute('placeholder', '');
 });
 
+const badgeEl = document.querySelector('header .badges')
+
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+new Swiper('.notice-line .swiper', {
+      direction: "vertical",  // 수직
+      autoplay: true,           // 자동
+      loop: true,                  // 반복
+});
+
+new Swiper('.promotion .swiper', {
+      slidesPerView: 3,       // 한번에 보여줄 슬라이드 개수
+      spaceBetween: 10,     // 슬라이드 사이 여백
+      centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+      autoplay: { delay: 4000 },     
+      loop: true,
+      pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true,
+      },
+      navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next',
+      }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide')
+  } else {
+    promotionEl.classList.remove('hide')
+  }
+})
